61e2fcab1fe18726fb14223c3c8d1aaba45fc59c
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-20 12:18:05 +0530

    Update and rename README.md to isdigit.py

---
+++ isdigit.py
@@ -0,0 +1,14 @@
+# isdigit
+def main():
+    pass
+    user=input().split()
+    l=list(n)
+    for number in l:
+        k= number.isdigit()
+    if (k==False):
+        print('no')
+    else:
+        print('yes')
+
+if __name__ == '__main__':
+    main()
